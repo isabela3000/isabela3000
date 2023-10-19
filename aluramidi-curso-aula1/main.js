6a0707f396ce86c4aca86de7dbab6f3e8c792793
isabela3000 <127404157+isabela3000@users.noreply.github.com>
2023-10-19 09:06:20 -0300

    Add files via upload

---
+++ aluramidi-curso-aula1/main.js
@@ -0,0 +1,10 @@
+// Pom----------------------
+    function tocaSomPom () {
+        document.querySelectors('#som-tecla-pom').play();
+    }
+
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+
